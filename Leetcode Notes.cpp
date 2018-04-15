415986dba6c5357ed19728198cd8c6006a467614
fejavu <fejavu@users.noreply.github.com>
2018-04-15 12:08:57 +0800

    debug

--- Leetcode Notes.cpp
+++ Leetcode Notes.cpp
@@ -54,20 +54,25 @@ string addBinary(string a, string b) {
     *You are given two non-empty linked lists representing two non-negative integers.
     *The digits are stored in reverse order and each of their nodes contain a single digit.
     *Add the two numbers and return it as a linked list.
-    */
+
     struct ListNode{
       int val;
       ListNode *next;
       ListNode(int x):val(x),next(NULL){}
-    };
+    };*/
 
+    static const auto __=[](){
+        ios::sync_with_stdio(false);//speed up to in and out
+        cin.tie(nullptr);
+        return nullptr;
+    }
     class solution{
     public:
-        ListNode* addTwoNumbers(ListNode* l1,ListNode* l2){
+        ListNode* addTwoNumbers(ListNode* l1,ListNode* l2) {
             ListNode prevHead(0),*pt = &prevHead;
             int carry = 0;
             while(l1||l2||carry){
-                int sum = (l1?l1->val:l1)+(l2?l2->val:l2)+carry;
+                int sum = (l1?l1->val:0)+(l2?l2->val:0) + carry;
                 carry = sum/10;
                 pt->next = ListNode(sum%10);
                 pt = pt->next;
