4e45c6420b1dfa5c6e38f50eb8a2b523cd5ff3db
fejavu <fejavu@users.noreply.github.com>
2018-04-25 23:52:42 +0800

    LeetCode

--- Leetcode Notes.cpp
+++ Leetcode Notes.cpp
@@ -311,4 +311,17 @@ vector<int> intersect2_1(vector<int>& nums1,vector<int>& nums2){
       }
       return false;
     }
+  /*
+  ================================================
+  = #50. Pow(x, n)
+  ================================================
+  */
+    double myPow(double x,int n){
+      if(n==0)  return  1;
+      double tmp = myPow(x,n/2);
+      if(n%2){
+        return n<0 ? 1/(x*tmp*tmp):x*tmp*tmp;
+      }else
+        return tmp*tmp;
+    }
 };
